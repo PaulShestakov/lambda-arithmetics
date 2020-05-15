9dbc703dcdc4bb4ace59499745202140e4ebc751
PaulShestakov <paulshestakov@icloud.com>
2020-05-16 01:43:27 +0300

    Update readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# 🍷 Need basic arithmetics, defined purely by functions?
+```
+const {_true, zero, succ, add, eq} = require("...")
+
+> eq(add(two)(three))(five) === _true
+
+// true
+```
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const { _true, _false, not, or, and } = require("./boolean");
+const { zero, is_zero, succ, pred } = require("./numbers");
+const { add, sub, eq } = require("./recursion");
+
+module.exports = {
+  _true,
+  _false,
+  not,
+  or,
+  and,
+  zero,
+  is_zero,
+  succ,
+  pred,
+  add,
+  sub,
+  eq
+};
