7bd4d08cfd6e2d644cd8a89d53a329de66429cd9
PaulShestakov <paulshestakov@icloud.com>
2020-08-24 01:32:18 +0300

    Added factorial example

--- src/recursion.js
+++ src/recursion.js
@@ -1,35 +1,39 @@
 const {cond, and} = require("./boolean")
 const {zero, is_zero, succ, pred} = require("./numbers")
 
+const Y =
+  f =>
+    (
+      s => arg => f ( s (s) ) (arg)
+    )(
+      s => arg => f ( s (s) ) (arg)
+    );
+
 const add1 = f => x => y => arg => cond ( x ) (f (succ ( x )) (pred ( y ))) (is_zero ( y )) (arg)
+
+const add = Y ( add1 )
 
 const sub1 = f => x => y => arg => cond (x) (f (pred (x)) (pred (y))) (is_zero ( y )) (arg)
 
-const recursive =
-  f =>
-    (
-      s =>
-        arg => f(s(s))(arg)
-    )(
-      s =>
-        arg => f(s(s))(arg)
-    );
-
-
-const add = recursive ( add1 )
-
-const sub = recursive ( sub1 )
+const sub = Y ( sub1 )
 
 const mult1 = f => x => y => arg => cond (zero) (add (x) (f (x) (pred (y)))) (is_zero ( y )) (arg)
 
-const mult = recursive ( mult1 )
+const mult = Y ( mult1 )
 
 const eq = x => y => and ( is_zero (sub (x) (y)) ) ( is_zero (sub (y) (x)) )
 
+const one = succ ( zero )
+
+const factorial1 = f => x => arg => cond (one) (mult (x) (f (pred(x)))) (is_zero(x)) (arg)
+
+const factorial = Y ( factorial1 )
+
 module.exports = {
-  recursive,
+  Y,
   add,
   sub,
   eq,
   mult,
+  factorial,
 }
--- src/recursion.spec.js
+++ src/recursion.spec.js
@@ -1,4 +1,4 @@
-const { add, sub, mult, eq } = require("./recursion");
+const { add, sub, mult, eq, factorial } = require("./recursion");
 const { zero, succ } = require("./numbers");
 const { _true } = require("./boolean");
 
@@ -11,21 +11,33 @@ const six = succ(five);
 
 describe("add", () => {
   test("0 + 0 = 0", () => {
-    expect(eq(add(zero)(zero))(zero)).toEqual(_true);
+    expect( eq(add(zero)(zero))(zero) ).toEqual(_true);
   });
   test("2 + 3 = 5", () => {
-    expect(eq(add(two)(three))(five)).toEqual(_true);
+    expect( eq(add(two)(three))(five) ).toEqual(_true);
   });
 });
 
 describe("sub", () => {
   test("5 - 4 = 1", () => {
-    expect(eq(sub(five)(four))(one)).toEqual(_true);
+    expect( eq(sub(five)(four))(one) ).toEqual(_true);
   });
 });
 
 describe("mult", () => {
   test("2 * 3 = 6", () => {
-    expect(eq(mult(two)(three))(six)).toEqual(_true);
+    expect( eq(mult(two)(three))(six) ).toEqual(_true);
   });
 });
+
+describe("factorial", () => {
+  test("0! = 1", () => {
+    expect( eq(factorial(zero))(one) ).toBe(_true);
+  })
+  test("1! = 1", () => {
+    expect( eq(factorial(one))(one) ).toBe(_true);
+  })
+  test("3! = 6", () => {
+    expect( eq(factorial(three))(six) ).toBe(_true);
+  })
+})
